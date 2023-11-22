cb811508d129dc602b622c72d71f27a844ca86c3
DevastatingRPG <devastatingrpg@gmail.com>
2023-11-22 23:42:53 +0530

    login and register work

--- server/server.js
+++ server/server.js
@@ -16,14 +16,66 @@ const connection = mysql.createConnection({
   host: env.SQL_SERVER,
   user: env.SQL_USER,
   password: env.SQL_PASSWORD,
-  database: env.SQL_DATABASE
+  database: env.SQL_DATABASE,
+  multipleStatements: true,
 });
 
-app.post('/', (req, res) => {
+app.get('/admin', (req, res) => {
+  const {func} = req.query;
+  
+})
+
+app.get('/booking', (req, res) => {
+  const {func, sid, vid} = req.query;
+  let avail, error;
+  const query = `CALL Show_Availability(${sid}, ${vid}, @avail, @error); SELECT @avail`;
+  connection.query(query, (err, rows, fields) => {
+    if (error){
+      console.error(error);
+      res.sendStatus(403);
+    }
+    res.send(rows[0][0]);
+  })
+})
+
+app.post('/booking', (req, res) => {
+  const {func} = req.query;
   const {seat, bid} = req.body;
   const query = `CALL InsertSeatNo(${seat}, ${bid})`;
-  res.send('Welcome to my server!');
+  connection.query(query, (err, rows, fields) => {
+    if (err){
+      console.error(err);
+      res.sendStatus(403);
+    }
+    res.sendStatus(200);
+  })
+  
 });
+
+app.post('/login', (req, res) => {
+  const {uid, password } = req.body;
+  const query = `CALL VerifyUserCredentials(\'${uid}\', \'${password}\', @verified, @error); SELECT @verified, @error;`;
+  connection.query(query, (err, rows, fields) => {
+    if (err){
+      console.error(err);
+      res.sendStatus(403);
+    }
+    res.send(rows[1][0]);
+  })
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, age, gender, mobile, uname, password } = req.body;
+  const query = `CALL InsertUser(\'${name}\', \'${email}\', ${age}, 
+  \'${gender}\', ${mobile}, \'${uname}\', \'${password}\')`;
+  connection.query(query, (err, rows, fields) => {
+    if (err){
+      console.error(err);
+      res.sendStatus(409);
+    }
+    res.sendStatus(200);
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
